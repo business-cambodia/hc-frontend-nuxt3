990e88abe0e3bb4270273187d146070858006e6d
NarithNR9 <narith.neymar.jr@gmail.com>
2024-06-26 10:33:43 +0700

    [add] navbar and footer

---
+++ components/Icons/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    @click="() => toggleDrawer"
+    class="w-6 h-6"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  toggleDrawer: Function,
+});
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
+  <LayoutsNavbar />
   <slot />
+  <LayoutsFooter />
 </template>
 
 <script setup lang="ts"></script>
