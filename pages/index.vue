14fe0f67ac2951e341b0d9c9d7ebd8a45df63f08
NarithNR9 <narith.neymar.jr@gmail.com>
2024-06-27 15:31:41 +0700

    [change] scrollbar style

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <div>
+    <img class="w-screen" src="https://healthy-cambodia.com/uploads/blog/News/5ad3c3ad58948288344079f66caa2447.jpg" alt="">
+    <img class="w-screen" src="https://healthy-cambodia.com/uploads/blog/News/5ad3c3ad58948288344079f66caa2447.jpg" alt="">
+  </div>
 </template>
 
 <script setup lang="ts"></script>
