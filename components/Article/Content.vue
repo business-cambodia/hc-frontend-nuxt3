eed42ac0aac20c24f075f179c03d34f1787adaa0
ZensYing <thakhmer855@gmail.com>
2026-06-05 09:19:28 +0700

    open domrey underlay

--- components/Article/Content.vue
+++ components/Article/Content.vue
@@ -92,14 +92,14 @@
                     class="article_body"
                   ></div>
                   <!--Mobile Underlay - Zone 1 [HIDDEN]-->
-                  <!-- <div id="gax-inpage-async-1700710878"></div> -->
+                  <div id="gax-inpage-async-1700710878"></div>
                 </div>
               </div>
               <div id="part-2">
                 <AdsBody :ads="secondParagraphAds" id="paragraph-2" :body="2" />
                 <div v-html="splitBody().secondPart" class="article_body"></div>
                 <!--Mobile Underlay - Zone 2 [HIDDEN]-->
-                <!-- <div id="gax-inpage-async-1706848594"></div> -->
+                <div id="gax-inpage-async-1706848594"></div>
               </div>
 
               <div id="part-3">
